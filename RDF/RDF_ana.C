3dc7199a706e2999d28868bb0f4e3d81d13d1b64
Gabriel A Rodriguez Linera <gabyrod7@gmail.com>
2023-12-27 11:04:05 -0500

    update rdf

--- RDF/RDF_ana.C
+++ RDF/RDF_ana.C
@@ -32,19 +32,24 @@ void RDF_ana(Int_t n_threads,string inf_name, string opf_name, Bool_t show_cut_r
 	
 	//3.1) Define some variables first:
 	auto rdf_variables = rdf.Define("fs", "flight_significance").Define("mmiss", "missing_p4_meas.M()").Define("missing_energy", "missing_p4_meas.E()")
-				.Define("mksp", "(ks_p4 + p_p4_kin).M()").Define("mklp", "(kl_p4 + p_p4_kin).M()")
-				.Define("ks_phi", "ks_p4_cm.Phi()").Define("p_z", "p_x4_kin.Z()")
+				.Define("mksp", "(ks_p4 + p_p4_kin).M()").Define("mklp", "(kl_p4 + p_p4_kin).M()").Define("mmiss2", "missing_p4_meas.M2()")
+				.Define("ks_phi", "ks_p4_cm.Phi()").Define("p_z", "p_x4_kin.Z()").Define("ks_costheta_cm", "ks_p4_cm.CosTheta()")
 				.Define("mpipp", "(pip_p4_kin + p_p4_kin).M()").Define("mpimp", "(pim_p4_kin + p_p4_kin).M()");
 
 	//3.2)Now apply cuts on the newly defined variables:
 	
 	auto rdf_cut = rdf_variables.Filter("mpipi > 0.48 && mpipi < 0.52 && mmiss > 0.3 && mmiss < 0.7 && fs > 6 && chisq_ndf < 2 && mandel_t > 0.2 && mandel_t < 0.5 && num_unused_tracks == 0 && num_unused_showers < 3");
+	auto rdf_cut4 = rdf_variables.Filter("mpipi > 0.48 && mpipi < 0.52 && mmiss > 0.3 && mmiss < 0.7 && mandel_t > 0.2 && mandel_t < 0.5 && num_unused_tracks == 0 && num_unused_showers < 3");
+	auto rdf_cut_v2 = rdf_variables.Filter("amptools_dat == 1 && mmiss > 0.3 && mmiss < 0.7 && fs > 6 && chisq_ndf < 2 && mandel_t > 0.2 && mandel_t < 0.5 && num_unused_tracks == 0 && num_unused_showers < 3");
+	// auto rdf_cut = rdf_variables.Filter("mpipi > 0.48 && mpipi < 0.52 && mmiss > 0.3 && mmiss < 0.7 && fs > 6 && chisq_ndf < 2 && mandel_t > 0.2 && mandel_t < 0.5 && num_unused_tracks == 0 && num_unused_showers < 3");
 	auto rdf_cut_sb = rdf_variables.Filter("((mpipi > 0.44 && mpipi < 0.46) || (mpipi > 0.54 && mpipi < 0.56)) && mmiss > 0.3 && mmiss < 0.7 && fs > 6 && chisq_ndf < 2 && mandel_t > 0.2 && mandel_t < 0.5 && num_unused_tracks == 0 && num_unused_showers < 3");
+	auto rdf_cut_sb2 = rdf_variables.Filter("amptools_bkg == true && mmiss > 0.3 && mmiss < 0.7 && fs > 6 && chisq_ndf < 2 && mandel_t > 0.2 && mandel_t < 0.5 && num_unused_tracks == 0 && num_unused_showers < 3");
+	auto rdf_cut_x = rdf_variables.Filter("mmiss > 0.3 && mmiss < 0.7 && fs > 6 && chisq_ndf < 2 && mandel_t > 0.2 && mandel_t < 0.5 && num_unused_tracks == 0 && num_unused_showers < 3");
 	//auto rdf_cut = rdf_variables.Filter("(p_z > 83 && p_z < 86) && mpipi > 0.48 && mpipi < 0.52 && mmiss > 0.3 && mmiss < 0.7 && fs > 4 && chisq_ndf < 4 && mandel_t > 0.15&& mandel_t < 1.5 && num_unused_tracks == 0 && num_unused_showers < 3");
 	//auto rdf_cut_sb = rdf_variables.Filter("(p_z > 83 && p_z < 86) && ((mpipi > 0.44 && mpipi < 0.46) || (mpipi > 0.54 && mpipi < 0.56)) && mmiss > 0.3 && mmiss < 0.7 && fs > 4 && chisq_ndf < 4 && mandel_t > 0.15&& mandel_t < 1.5 && num_unused_tracks == 0 && num_unused_showers < 3");
 	auto rdf_cut2 = rdf_variables.Filter("mpipi > 0.48 && mpipi < 0.52 && mmiss > 0.4 && mmiss < 0.6 && fs > 6 && chisq_ndf < 2 && num_unused_tracks == 0 && num_unused_showers < 3");
 	auto rdf_cut2_sb = rdf_variables.Filter("((mpipi > 0.44 && mpipi < 0.46) || (mpipi > 0.54 && mpipi < 0.56)) && mmiss > 0.4 && mmiss < 0.6 && fs > 6 && chisq_ndf < 2 && num_unused_tracks == 0 && num_unused_showers < 3");
-	auto rdf_cut3 = rdf_variables.Filter("mmiss > 0.3 && mmiss < 0.7 && fs > 4 && chisq_ndf < 4 && num_unused_tracks == 0 && num_unused_showers < 3 && mandel_t > 0.15 && mandel_t < 1.5");
+	auto rdf_cut3 = rdf_variables.Filter("mmiss > 0.3 && mmiss < 0.7 && fs > 6 && chisq_ndf < 2 && mandel_t > 0.2 && mandel_t < 0.5 && num_unused_tracks == 0 && num_unused_showers < 3");
 	auto rdfChiSqNdf_cut = rdf_variables.Filter("mmiss > 0.4 && mmiss < 0.6 && fs > 3 && mandel_t > 0.1 && mandel_t < 0.5 && num_unused_tracks == 0");
 	auto rdfFlightSignificance_cut = rdf_variables.Filter("mmiss > 0.4 && mmiss < 0.6 && chisq_ndf < 4 && mandel_t > 0.1 && mandel_t < 0.5 && num_unused_tracks == 0");
 	auto rdfNumUnusedTracks_cut = rdf_variables.Filter("mmiss > 0.4 && mmiss < 0.6 && chisq_ndf < 2 && fs > 6 && mandel_t > 0.1 && mandel_t < 0.5");
@@ -58,6 +63,10 @@ void RDF_ana(Int_t n_threads,string inf_name, string opf_name, Bool_t show_cut_r
 	cout <<"Set up histograms..."<< endl;
 	
 	//4.1) Histograms
+	auto kscosThetaCM = rdf_cut.Histo1D({"kscosThetaCM", ";K_{S} cos#theta CoM;Counts", 100, -1, 1}, "ks_costheta_cm", "accidental_weight");
+	auto cos_mkskl = rdf_cut.Histo2D({"cos_mkskl", ";M(K_{S}K_{L});K_{S} cos#theta CoM",  50, 1.10, 2.0, 40, -1, 1}, "mkskl", "ks_costheta_cm", "accidental_weight");
+	auto van_hove = rdf_cut.Histo2D({"van_hove", ";X;Y",  40, -4, 4, 40, -4, 4}, "vanHove_x", "vanHove_y", "accidental_weight");
+
 	auto im_kskl = rdf_cut.Histo1D({"im_kskl", ";M(K_{S}K_{L});Counts", 50 , 1.00, 2.00}, "mkskl", "accidental_weight");
 	auto im_kskl2 = rdf_cut.Filter("missing_energy > 2").Histo1D({"im_kskl2", ";M(K_{S}K_{L});Counts", 60 , 1.00, 2.20}, "mkskl", "accidental_weight");
 	auto im_ksp = rdf_cut.Histo1D({"im_ksp", ";M(K_{S}p);Counts", 100, 1.00, 4.00}, "mksp", "accidental_weight");
@@ -65,10 +74,17 @@ void RDF_ana(Int_t n_threads,string inf_name, string opf_name, Bool_t show_cut_r
 	auto im_pipp = rdf_cut.Histo1D({"im_pipp", ";M(#pi^{+}p);Counts", 100, 1.00, 4.00}, "mpipp", "accidental_weight");
 	auto im_pimp = rdf_cut.Histo1D({"im_pimp", ";M(#pi^{-}p);Counts", 100, 1.00, 4.00}, "mpimp", "accidental_weight");
 	auto im_miss = rdf_cut.Histo1D({"im_miss", ";Missing Mass (GeV);Counts", 100, 0.00, 1.00}, "mmiss", "accidental_weight");
-	auto im_pipi = rdf_cut3.Filter("mkskl > 1.005 & mkskl < 1.05").Histo1D({"im_pipi", ";M(#pi#pi) (GeV);Counts", 150, 0.40, 0.55}, "mpipi", "accidental_weight");
+	auto im_pipi = rdf_cut3.Histo1D({"im_pipi", ";M(#pi#pi) (GeV);Counts", 200, 0.40, 0.60}, "mpipi", "accidental_weight");
+	auto im_pipi2 = rdf_cut_sb2.Histo1D({"im_pipi2", ";M(#pi#pi) (GeV);Counts", 200, 0.40, 0.60}, "mpipi", "Weight");
+	auto im_pipi3 = rdf_cut_sb2.Filter("is_in_time == true").Histo1D({"im_pipi3", ";M(#pi#pi) (GeV);Counts", 200, 0.40, 0.60}, "mpipi", "Weight");
+	auto im_pipi4 = rdf_cut_sb2.Filter("is_in_time == false").Histo1D({"im_pipi4", ";M(#pi#pi) (GeV);Counts", 200, 0.40, 0.60}, "mpipi", "Weight");
+	auto im_pipi5 = rdf_cut_v2.Histo1D({"im_pipi5", ";M(#pi#pi) (GeV);Counts", 200, 0.40, 0.60}, "mpipi", "accidental_weight");
 
 	auto miss_energy = rdf_cut.Histo1D({"miss_energy", ";Missing Energy (GeV);Counts", 100, 0.00, 10.00}, "missing_energy", "accidental_weight");
 	auto miss_energy_sb = rdf_cut_sb.Histo1D({"miss_energy_sb", ";Missing Energy (GeV);Counts", 100 , 0.00, 10.0}, "missing_energy", "accidental_weight");
+
+	auto im_miss2 = rdf_cut.Histo1D({"im_miss2", ";Missing Mass^{2} (GeV);Counts", 100, 0.00, 2.00}, "mmiss2", "accidental_weight");
+	auto im_miss2_sb = rdf_cut_sb.Histo1D({"im_miss2_sb", ";Missing Mass (GeV);Counts", 100, 0.00, 2.00}, "mmiss2", "accidental_weight");
 
 	auto im_kskl_sb = rdf_cut_sb.Histo1D({"im_kskl_sb", ";M(K_{S}p);Counts", 50 , 1.00, 2.00}, "mkskl", "accidental_weight");
 	auto im_ksp_sb = rdf_cut_sb.Histo1D({"im_ksp_sb", ";M(K_{S}p);Counts", 100, 1.00, 4.00}, "mksp", "accidental_weight");
@@ -100,12 +116,32 @@ void RDF_ana(Int_t n_threads,string inf_name, string opf_name, Bool_t show_cut_r
 	auto h2_mkskl_mpipp = rdf_cut.Histo2D({"h2_mkskl_mpipp", ";M(K_{S}K_{L});M(#pi^{+}p)", 200, 0.98, 2.98, 100, 1.0, 4.0}, "mkskl", "mpipp", "accidental_weight");
 	auto h2_mkskl_mpimp = rdf_cut.Histo2D({"h2_mkskl_mpimp", ";M(K_{S}K_{L});M(#pi^{-}p)", 200, 0.98, 2.98, 100, 1.0, 4.0}, "mkskl", "mpimp", "accidental_weight");
 
-	auto h2_mkskl_coshx = rdf_cut.Histo2D({"h2_mkskl_coshx", ";M(K_{S}K_{L});cos(#theta_{hel})", 200, 0.98, 2.98, 100, -1.0, 1.0}, "mkskl", "cos_hel_ks", "accidental_weight");
+	auto h2_mkskl_coshx = rdf_cut.Histo2D({"h2_mkskl_coshx", ";M(K_{S}K_{L});cos(#theta_{hel})", 50, 1.00, 2.0, 40, -1.0, 1.0}, "mkskl", "cos_hel_ks", "accidental_weight");
+	auto h2_mkskl_coshx_sb = rdf_cut_sb.Histo2D({"h2_mkskl_coshx_sb", ";M(K_{S}K_{L});cos(#theta_{hel})", 50, 1.00, 2.0, 40, -1.0, 1.0}, "mkskl", "cos_hel_ks", "accidental_weight");
+	auto h2_mkskl_coshx2 = rdf_cut_x.Filter("amptools_dat == true").Histo2D({"h2_mkskl_coshx2", ";M(K_{S}K_{L});cos(#theta_{hel})", 50, 1.00, 2.0, 40, -1.0, 1.0}, "mkskl", "cos_hel_ks", "Weight");
+	auto h2_mkskl_coshx_sb2 = rdf_cut_x.Filter("amptools_bkg == true").Histo2D({"h2_mkskl_coshx_sb2", ";M(K_{S}K_{L});cos(#theta_{hel})", 50, 1.00, 2.0, 40, -1.0, 1.0}, "mkskl", "cos_hel_ks", "Weight");
+
+	auto h2_mpipi_MissingEnergy = rdf_cut3.Filter("mandel_t < 0.5").Histo2D({"h2_mpipi_MissingEnergy", ";M(#pi^{+}#pi^{-});Missing Energy (GeV)", 150, 0.40, 0.55, 100, 0.0, 10.}, "mpipi", "missing_energy", "accidental_weight");
+	auto h2_mpipi_MissingEnergy2 = rdf_cut3.Filter("mandel_t > 0.5").Histo2D({"h2_mpipi_MissingEnergy2", ";M(#pi^{+}#pi^{-});Missing Energy (GeV)", 150, 0.40, 0.55, 100, 0.0, 10.}, "mpipi", "missing_energy", "accidental_weight");
+
+	auto h2_kskl_MissingEnergy = rdf_cut.Filter("mandel_t < 0.5").Histo2D({"h2_kskl_MissingEnergy", ";M(K_{S}K_{L});Missing Energy (GeV)", 100, 0.98, 1.98, 100, 0.0, 10.}, "mkskl", "missing_energy", "accidental_weight");
+	auto h2_kskl_MissingEnergy2 = rdf_cut.Filter("mandel_t > 0.5").Histo2D({"h2_kskl_MissingEnergy2", ";M(K_{S}K_{L});Missing Energy (GeV)", 100, 0.98, 1.98, 100, 0.0, 10.}, "mkskl", "missing_energy", "accidental_weight");
+
+	auto h2_MissingEnergy_mandelt = rdf_cut2.Histo2D({"h2_MissingEnergy_mandelt", ";Missing Energy (GeV);-t (GeV^{2})", 100, 0.0, 10., 50, 0, 1}, "missing_energy", "mandel_t", "accidental_weight");
+
+	auto h2_fs_chisq = rdf_cut4.Histo2D({"h2_fs_chisq", ";FS;#chi^2", 70, 3, 10, 60, 0, 6}, "fs", "chisq_ndf", "accidental_weight");
 
 	cout <<" "<< endl;
 	
 	//5.) Write everything to a file:
 	cout <<"Write results to file: " << opf_name << endl;
+
+	van_hove->Write();
+	kscosThetaCM->Write();
+	cos_mkskl->Write();
+
+	im_miss2->Write();
+	im_miss2_sb->Write();
 
 	miss_energy->Write();
 	miss_energy_sb->Write();
@@ -118,6 +154,10 @@ void RDF_ana(Int_t n_threads,string inf_name, string opf_name, Bool_t show_cut_r
 	im_pimp->Write();
 	im_miss->Write();
 	im_pipi->Write();
+	im_pipi2->Write();
+	im_pipi3->Write();
+	im_pipi4->Write();
+	im_pipi5->Write();
 
 	im_kskl_sb->Write();
 	im_ksp_sb->Write();
@@ -149,13 +189,19 @@ void RDF_ana(Int_t n_threads,string inf_name, string opf_name, Bool_t show_cut_r
 	h2_mkskl_mpimp->Write();
 
 	h2_mkskl_coshx->Write();
+	h2_mkskl_coshx_sb->Write();
+	h2_mkskl_coshx2->Write();
+	h2_mkskl_coshx_sb2->Write();
 
+	h2_MissingEnergy_mandelt->Write();
 
-	auto chisq_sig = rdf_variables.Filter("mpipi > 0.48 && mpipi < 0.52").Histo1D({"chisq_sig", ";M(K_{S}p);Counts",  60, 0, 6}, "chisq_ndf", "accidental_weight");
-	auto chisq_bkg = rdf_variables.Filter("(mpipi > 0.44 && mpipi < 0.46) || (mpipi > 0.54 && mpipi < 0.56)").Histo1D({"chisq_bkg", ";M(K_{S}p);Counts",  60, 0, 6}, "chisq_ndf", "accidental_weight");
+	h2_mpipi_MissingEnergy->Write();
+	h2_mpipi_MissingEnergy2->Write();
 
-	chisq_sig->Write();
-	chisq_bkg->Write();
+	h2_kskl_MissingEnergy->Write();
+	h2_kskl_MissingEnergy2->Write();
+
+	h2_fs_chisq->Write();
 
 	out_file->Write();
 	out_file->Close();
--- RDF/runRDF.sh
+++ RDF/runRDF.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 ### Produce histograms to be analyzed with RDataFrames
-root -l -b -q 'RDF_ana.C(20, "../DSelector/ftree_dat_gluex1.root", "hist_dat.root", 0)'
-root -l -b -q 'RDF_ana.C(20, "../DSelector/ftree_acc_gluex1.root", "hist_acc.root", 0)'
+# root -l -b -q 'RDF_ana.C(16, "../DSelector/ftrees/ftree_dat_sp17.root", "hist_dat.root", 0)'
+# root -l -b -q 'RDF_ana.C(16, "../DSelector/ftrees/ftree_acc_sp17.root", "hist_acc.root", 0)'
+
+root -l -b -q 'RDF_ana.C(16, "../DSelector/ftrees/ftree_dat_gluex1.root", "hist_dat.root", 0)'
+root -l -b -q 'RDF_ana.C(16, "../DSelector/ftrees/ftree_acc_gluex1.root", "hist_acc.root", 0)'

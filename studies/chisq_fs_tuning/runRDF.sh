2779a5cf10565dd2b853995c09ba13887af56496
Gabriel A Rodriguez Linera <gabyrod7@gmail.com>
2024-06-25 07:37:12 -0400

    Cuts study

---
+++ studies/chisq_fs_tuning/runRDF.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+### Produce histograms to be analyzed with RDataFrames
+root -l -b -q 'RDF_ana.C(16, "/d/grid15/gabyrod7/analysis/ksmisskl_gabyrod_gluex1_PWA/DSelector/ftrees/ftree_dat_gluex1.root", "hist_dat_gluex1.root", 0)'

5a0832c5717f546920c1c14039572643dd490059
Gabriel A Rodriguez Linera <gabyrod7@gmail.com>
2024-06-25 07:29:06 -0400

    Don't remmeber why I made these plots

---
+++ kinematics/RDF_ana.C
@@ -0,0 +1,129 @@
+std::string set_cuts(std::map<std::string, std::string> cuts_list, std::pair<std::string, std::string> change_cut = {"", ""});
+
+void RDF_ana(Int_t n_threads,string inf_name, string opf_name, Bool_t show_cut_report) {	
+	cout<<" "<< endl;
+	cout<<"Run RDataFrame Analysis"<< endl;
+	cout<<" "<< endl;
+    
+	if(n_threads > 0.0)	ROOT::EnableImplicitMT(n_threads);
+	
+	TStopwatch watch;
+	
+	watch.Reset();
+	watch.Start();
+	
+	cout <<"Load data and set ROOT DataFrame..."<< endl;
+	
+	//1.) Define the chain:
+	TChain my_chain("kskl");
+	my_chain.Add(inf_name.c_str());
+	Int_t nEvents = my_chain.GetEntries();
+   	cout << "Events to process " << nEvents << endl; 
+
+	TFile *out_file = new TFile(opf_name.c_str(),"RECREATE");
+
+	//2.) Set the RDataFrame:
+	ROOT::RDataFrame rdf(my_chain);
+	
+	cout <<"...done!"<< endl;
+	cout <<" "<< endl;
+	
+	cout <<"Define variables and cuts..."<< endl;
+	
+	//3.) Define important variables and analysis conditions
+	
+	//3.1) Define some variables first:
+	auto rdf_variables = rdf.Define("fs", "flight_significance").Define("mmiss", "missing_p4_meas.M()").Define("missing_energy", "missing_p4_meas.E()")
+				.Define("mksp", "(ks_p4 + p_p4_kin).M()").Define("mklp", "(kl_p4 + p_p4_kin).M()").Define("mmiss2", "missing_p4_meas.M2()")
+				.Define("ks_phi", "ks_p4_cm.Phi()").Define("p_z", "p_x4_kin.Z()").Define("ks_costheta_cm", "ks_p4_cm.CosTheta()")
+				.Define("mpipp", "(pip_p4_kin + p_p4_kin).M()").Define("mpimp", "(pim_p4_kin + p_p4_kin).M()")
+				.Define("ks_mom", "ks_p4.Vect().Mag()");
+
+	//3.2)Now apply cuts on the newly defined variables:
+	
+	std::map<std::string, std::string> cuts_list = {
+		// {"mkskl", "mkskl > 1.1 && mkskl < 2.00"},
+		{"mmiss", "missing_mass > 0.3 && missing_mass < 0.7"},
+		// {"mandel_t", "mandel_t > 0.2 && mandel_t < 0.5"},
+		{"fs", "flight_significance > 6"},
+		{"chisq", "chisq_ndf < 2"},
+		{"ntracks", "num_unused_tracks == 0"},
+		{"nshowers", "num_unused_showers < 3"},
+		{"proton_z_vertex", "proton_z_vertex > 52 && proton_z_vertex < 78"},
+		{"beam_energy", "beam_energy > 8.2 && beam_energy < 8.8"}};
+
+	string cuts = set_cuts(cuts_list);
+	string signal = "(mpipi > 0.48 && mpipi < 0.52)";
+	string sideband = "((mpipi > 0.43 && mpipi < 0.45) || (mpipi > 0.55 && mpipi < 0.57))";
+
+	auto rdf_cut = rdf_variables.Filter(cuts);
+
+	cout <<"...done!"<< endl;
+	cout <<" "<< endl;
+	
+	//4.) Define Histograms:
+	
+	cout <<"Set up histograms..."<< endl;
+	
+	//4.1) Histograms
+	auto h2_fs_chisq = rdf_cut.Histo2D({"h2_fs_chisq", ";K_{S} momenntum;-t", 100, 0, 10, 10, 0, 1}, "ks_mom", "mandel_t", "accidental_weight");
+	auto h2_pathLength_chisq = rdf_cut.Histo2D({"h2_pathLength_chisq", ";Path Length;-t", 100, 0, 100, 10, 0, 1}, "path_length", "mandel_t", "accidental_weight");
+
+	cout <<" "<< endl;
+	
+	//5.) Write everything to a file:
+	cout <<"Write results to file: " << opf_name << endl;
+
+	h2_fs_chisq->Write();
+	h2_pathLength_chisq->Write();
+
+	out_file->Write();
+	out_file->Close();
+	
+	cout <<"...done!"<< endl;
+	cout <<" "<< endl;
+	
+	watch.Stop();
+	Double_t ana_time = watch.RealTime();
+
+	if(n_threads > 0 && show_cut_report)
+		cout << "Most set n_threads to 0 to view cut report" << endl;
+
+	if(n_threads == 0 && show_cut_report){
+		cout <<"  "<< endl;
+		cout <<"Cut Report:"<< endl;
+		auto allCutsReport = rdf_cut.Report();
+		allCutsReport->Print();
+		cout <<"  "<< endl;
+	}
+
+	Double_t report_time = 0.0;
+	TString ana_time_unit = "s";
+	//----------------------------------------------
+	if(ana_time < 60.0){
+	    report_time = ana_time;
+	}else if(ana_time >= 60.0 && ana_time < 3600.0){
+	    report_time = ana_time / 60.0;
+	    ana_time_unit = "min";
+	}else if(ana_time >= 3600.0){
+	    report_time = ana_time / 3600.0;
+	    ana_time_unit = "h";
+	}
+	//----------------------------------------------
+	
+	cout <<"Processed "<<nEvents<<" events in: "<<report_time<<ana_time_unit<< endl;
+	cout <<"Have a great day!"<< endl;
+	cout <<" "<< endl;
+}
+
+std::string set_cuts(std::map<std::string, std::string> cuts_list, std::pair<std::string, std::string> change_cut = {"", ""}) {
+	std::string cuts = "";
+
+	for(auto it = cuts_list.begin(); it != cuts_list.end(); ++it) {
+		if(it->first == change_cut.first)	cuts += change_cut.second + " && ";
+		else								cuts += it->second + " && ";
+	}
+	cuts.erase(cuts.size()-4, 4); // remove last " && "
+
+	return cuts;
+}
